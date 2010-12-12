c2d9df693c2fd817eea825c18dda6fb4956b0a04
Stefan Glase <stefan.glase@googlemail.com>
2010-12-12 17:02:38 +0100

    implemented numbers 1 to 10

--- caesar-lottery-101212/src/main/groovy/caesarlottery/CaesarLottery.groovy
+++ caesar-lottery-101212/src/main/groovy/caesarlottery/CaesarLottery.groovy
@@ -2,9 +2,22 @@ package caesarlottery
 
 class CaesarLottery {
     
+    /**
+     * Convert an Arabic numeral to Roman numeral.
+     *
+     * @param number    an Arabic numeral
+     * @return          the Roman numeral equivalent to the Arabic numeral
+     */
     String convert (int number) {
-        // TODO implement solution
-        return null;
+        def result = ""
+        [10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"].each { arabic, roman ->
+            println "$arabic und $roman .." 
+            while (number - arabic >= 0) {
+                result += roman
+                number -= arabic
+            }
+        }
+        result
     }
     
 }
--- caesar-lottery-101212/src/test/groovy/caesarlottery/CaesarLotteryTests.groovy
+++ caesar-lottery-101212/src/test/groovy/caesarlottery/CaesarLotteryTests.groovy
@@ -4,9 +4,44 @@ import org.junit.Test
 
 class CaesarLotteryTests {
     
-    @Test
-    void int1shouldreturnI() {
+    @Test void shouldTransformArabian1ToRomanI() {
         assert new CaesarLottery().convert(1) == "I"
+    }
+
+    @Test void shouldTransformArabian2ToRomanII() {
+        assert new CaesarLottery().convert(2) == "II"
+    }
+    
+    @Test void shouldTransformArabian3ToRomanIII() {
+        assert new CaesarLottery().convert(3) == "III"
+    }
+    
+    @Test void shouldTransformArabian4ToRomanIV() {
+        assert new CaesarLottery().convert(4) == "IV"
+    }
+    
+    @Test void shouldTransformArabian5ToRomanV() {
+        assert new CaesarLottery().convert(5) == "V"
+    }
+
+    @Test void shouldTransformArabian6ToRomanVI() {
+        assert new CaesarLottery().convert(6) == "VI"
+    }
+
+    @Test void shouldTransformArabian7ToRomanVII() {
+        assert new CaesarLottery().convert(7) == "VII"
+    }
+
+    @Test void shouldTransformArabian8ToRomanVIII() {
+        assert new CaesarLottery().convert(8) == "VIII"
+    }
+
+    @Test void shouldTransformArabian9ToRomanIX() {
+        assert new CaesarLottery().convert(9) == "IX"
+    }
+
+    @Test void shouldTransformArabian10ToRomanX() {
+        assert new CaesarLottery().convert(10) == "X"
     }
     
 }
