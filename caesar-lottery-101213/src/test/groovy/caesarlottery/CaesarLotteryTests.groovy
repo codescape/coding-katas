632daee3331dc39151c74958b04b4b897c968ab6
Stefan Glase <stefan.glase@googlemail.com>
2010-12-13 20:24:39 +0100

    added test data up to arabic numeral 20

--- caesar-lottery-101213/src/test/groovy/caesarlottery/CaesarLotteryTests.groovy
+++ caesar-lottery-101213/src/test/groovy/caesarlottery/CaesarLotteryTests.groovy
@@ -4,7 +4,8 @@ import org.junit.Test
 
 class CaesarLotteryTests {
 
-    def conversions = [1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X", 11: "XI", 12: "XII", 13: "XIII", 14: "XIV", 15: "XV"]
+    def conversions = [1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X", 
+        11: "XI", 12: "XII", 13: "XIII", 14: "XIV", 15: "XV", 16: "XVI", 17: "XVII", 18: "XVIII", 19: "XIX", 20: "XX"]
 
     @Test
     void convertArabinNumeralsInRomanNumerals() {
