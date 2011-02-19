583a80ecf3774e8ee9dd4e606315b215aa8e5938
Stefan Glase <stefan.glase@googlemail.com>
2011-02-19 18:52:11 +0100

    works for numbers divisible by 5

--- fizz-buzz-110219/src/main/groovy/fizzbuzz/FizzBuzz.groovy
+++ fizz-buzz-110219/src/main/groovy/fizzbuzz/FizzBuzz.groovy
@@ -3,7 +3,7 @@ package fizzbuzz
 class FizzBuzz {
     
     static def tell(number) {
-        number % 3 == 0 ? "fizz" : number
+        number % 3 == 0 ? "fizz" : number % 5 == 0 ? "buzz" : number
     }
     
 }
--- fizz-buzz-110219/src/test/groovy/fizzbuzz/FizzBuzzSpecs.groovy
+++ fizz-buzz-110219/src/test/groovy/fizzbuzz/FizzBuzzSpecs.groovy
@@ -18,4 +18,12 @@ class FizzBuzzSpecs extends Specification {
         number << [3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 96, 99]
     }
     
+    def "numbers divisible by 5 but not divisible by 3 become the word buzz"() {
+        expect:
+        FizzBuzz.tell(number) == "buzz"
+        
+        where:
+        number << [5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100]
+    }
+    
 }
