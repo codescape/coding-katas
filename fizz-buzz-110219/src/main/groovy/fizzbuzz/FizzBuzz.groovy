d0ffad7216b8c67789f3f2da789b7e4b886624f0
Stefan Glase <stefan.glase@googlemail.com>
2011-02-20 00:17:35 +0100

    formatting code

--- fizz-buzz-110219/src/main/groovy/fizzbuzz/FizzBuzz.groovy
+++ fizz-buzz-110219/src/main/groovy/fizzbuzz/FizzBuzz.groovy
@@ -2,7 +2,7 @@ package fizzbuzz
 
 class FizzBuzz {
     
-    static def tell(number) {
+    static tell(number) {
         number % 15 == 0 ? "fizzbuzz" : number % 3 == 0 ? "fizz" : number % 5 == 0 ? "buzz" : number
     }
     
